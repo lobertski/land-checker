2142a6997162a578cd2a5145eaa9320a332454ad
Robert Joshua Lopez <lopezrobertjoshua@gmail.com>
2024-01-17 08:51:24 +0800

    Added Marker Icon and Fixed center map position

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,12 +1,22 @@
 import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
 import { IMap } from "../../types";
 import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet";
+import L from "leaflet";
+
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 export const Map = ({ properties }: IMap) => {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <MapContainer center={[45.4, -75.7]} zoom={12}>
+      <MapContainer center={[-37.8405, 146.3323]} zoom={7}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
